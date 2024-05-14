7e4c19744af0df6c88fc555c346e5b69b2b0ab58
Vincent Nys <vincentnys@gmail.com>
2024-05-14 09:47:33 +0200

    Allows image uploading. GET and POST methods are handled as expected.

--- image-processing-ui/handler.js
+++ image-processing-ui/handler.js
@@ -1,8 +1,30 @@
 'use strict'
 
 module.exports = async (event, context) => {
-  return context
-    .headers({"Content-Type": "text/html"})
-    .status(200)
-    .succeed("<h1>Kijk!</h1><p>HTML!</p>")
+  console.debug(event);
+  if (event.method === 'GET') {
+      return context
+        .headers({"Content-Type": "text/html"})
+        .status(200)
+        .succeed(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+            <form id="uploadForm" action="#" method="POST" enctype="multipart/form-data">
+              <input id="fileInput" type="file" name="file" />
+              <input type="submit" value="process image" />
+            </form>
+</body>
+</html>`)
+  }
+  else {
+      return context
+        .headers({"Content-Type": "text/html"})
+        .status(200)
+        .succeed("<h1>Kijk!</h1><p>HTML!</p>")
+  }
 }
